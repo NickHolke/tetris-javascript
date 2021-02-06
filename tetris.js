62d4cbd8d1828fd8ea2fecb32339665cc60c16ef
Nick Holke <nickholke@gmail.com>
2021-02-06 10:59:08 -0800

    Added event listeners and created arena to store position of player

--- tetris.js
+++ tetris.js
@@ -3,13 +3,30 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-
-
 const matrix = [
   [0, 0, 0],
   [1, 1, 1],
   [0, 1, 0]
 ];
+
+const player = {
+  matrix: matrix,
+  pos: {x:5, y:5}
+}
+
+let lastTime = 0;
+let dropCounter = 0;
+const dropInterval = 1000;
+
+const arena = createMatrix(12, 20);
+
+function createMatrix(width, height) {
+  const matrix = [];
+  while (height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+}
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -22,9 +39,14 @@ function drawMatrix(matrix, offset) {
   })
 }
 
-const player = {
-  matrix: matrix,
-  pos: {x:5, y:5}
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    })
+  })
 }
 
 function draw() {
@@ -34,22 +56,32 @@ function draw() {
   drawMatrix(player.matrix, player.pos);
 }
 
-let lastTime = 0;
-let dropCounter = 0;
-const dropInterval = 1000;
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
 
 function update(time = 0) {
   let deltaTime = time - lastTime;
   lastTime = time;
   dropCounter += deltaTime;
-  
-  if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
-  }
 
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
   draw();
   requestAnimationFrame(update)
 }
 
 update();
+
+document.addEventListener('keydown', event => {
+  console.log(event)
+  if (event.key === "ArrowLeft") {
+    player.pos.x--;
+  } else if (event.key === "ArrowRight") {
+    player.pos.x++;
+  } else if (event.key === "ArrowDown") {
+    playerDrop();
+  }  
+})
